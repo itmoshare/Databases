3fb2b96b0519c375553c7f87abf5f4ea6d767f22
Дмитрий Мохнактин <dmitriy.mokhnatkin@gmail.com>
2017-05-09 12:46:08 +0300

    Routes controller

--- src/RestApi/RestApi/Controllers/RoutesController.cs
+++ src/RestApi/RestApi/Controllers/RoutesController.cs
@@ -17,7 +17,35 @@ namespace RestApi.Controllers
             _graphClient = graphClient;
         }
 
-        public IEnumerable<UnitRoute> GetRoutes(int unitStopId, int routeNumber)
+        /// <summary>
+        /// Create relation between bus stop and route number
+        /// </summary>
+        [HttpPut("{unitStopId}/{routeNumber}")]
+        public IActionResult CreateTimeTableItem(int unitStopId, string routeNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Delete relation between bus stop and route number
+        /// </summary>
+        [HttpDelete]
+        public IActionResult DeleteTimeTableItem([FromBody]TimeTableItem timeTableItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Get relations by unit stop and route id.
+        /// </summary>
+        [HttpGet("{unitStopId>/{routeNumber}")]
+        public IEnumerable<TimeTableItem> GetRoutes(int unitStopId, string routeNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateTimeTableItem([FromBody]TimeTableItem lastItem, [FromBody]TimeTableItem newItem)
         {
             throw new NotImplementedException();
         }
